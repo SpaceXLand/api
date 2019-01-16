69036e200ecefe454d09698c5e99df74890319e9
Carlos Rufo <sw@carlosrj.com>
2019-01-16 18:39:33 +0100

    add swagger sofa config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import sofa from 'sofa-api';
+import sofa, { OpenAPI } from 'sofa-api';
 import schema from './schema';
 import context from './context';
 import { getDB } from './context/db';
@@ -26,16 +26,29 @@ import { getDB } from './context/db';
     app
   });
 
+  const openApi = OpenAPI({
+    schema,
+    info: {
+      title: 'SpaceX Rest API'
+    }
+  });
+
   app.use(
     '/rest',
     sofa({
       schema,
-      context: ctx
+      context: ctx,
+      onRoute(info) {
+        openApi.addRoute(info);
+      }
     })
   );
   app.get('/', (_, res) => {
     res.redirect(graphql.graphqlPath);
   });
+
+  // writes every recorder route
+  openApi.save('./swagger.yml');
 
   app.listen({ port }, () => {
     console.log(`🚀  Server ready http://localhost:${port}`);
