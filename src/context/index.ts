207375e08b200aa51db060ca7d5cc182bcd40c73
Carlos Rufo <sw@carlosrj.com>
2019-01-19 10:38:42 +0100

    remove project utils

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,5 @@
-import { find, limit, offset, order, project, sort } from '../utils';
+import { find, limit, offset, order, sort } from '../utils';
 
-const context = { find, limit, offset, order, project, sort };
+const context = { find, limit, offset, order, sort };
 
 export default context;
