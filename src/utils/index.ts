66444b8827cabe1bd490176edfa06b4029362761
Carlos Rufo <sw@carlosrj.com>
2019-01-19 10:40:32 +0100

    remove project utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,4 @@ export { default as find } from './find';
 export { default as limit } from './limit';
 export { default as offset } from './offset';
 export { default as order } from './order';
-export { default as project } from './project';
 export { default as sort } from './sort';
