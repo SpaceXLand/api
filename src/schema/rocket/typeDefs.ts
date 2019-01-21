2a3fb521a46cdf6ae6affaced868ff62c7ee6282
Carlos Rufo <sw@carlosrj.com>
2019-01-21 12:35:32 +0100

    remove rocket @rateLimit

--- src/schema/rocket/typeDefs.ts
+++ src/schema/rocket/typeDefs.ts
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 const typeDefs = gql`
   extend type Query {
     rockets(limit: Int, offset: Int): [Rocket]
-    rocket(id: ID!): Rocket @rateLimit
+    rocket(id: ID!): Rocket
   }
 
   type Rocket {
