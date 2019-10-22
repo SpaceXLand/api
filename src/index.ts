79c0cd3c2a284c4937931f8b22ea39dc8b901c4a
Carlos Rufo <sw@carlosrj.com>
2019-10-22 17:41:55 +0800

    merge schemas

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express from 'express';
-import { getDB } from './context/db';
-import schema from './schema';
-import context from './context';
-import graphql from './servers/graphql';
-import rest from './servers/rest';
+require("dotenv").config();
+import express from "express";
+import { getDB } from "./context/db";
+import getSchema from "./schema";
+import context from "./context";
+import graphql from "./servers/graphql";
+import rest from "./servers/rest";
 
 (async () => {
   const port = process.env.PORT || 4000;
   const app = express();
+  const schema = await getSchema();
   const db = await getDB();
 
   const config = {
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,14 +1,44 @@
-import { makeExecutableSchema } from 'apollo-server-express';
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
-import { rateLimit } from '../utils';
+// import { makeExecutableSchema } from "apollo-server-express";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
+import { rateLimit } from "../utils";
+import {
+  makeExecutableSchema,
+  makeRemoteExecutableSchema,
+  introspectSchema,
+  mergeSchemas
+} from "graphql-tools";
+import fetch from "node-fetch";
+import { HttpLink } from "apollo-link-http";
 
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-  schemaDirectives: {
-    rateLimit
-  }
+const uri = process.env.X_HASURA_URL;
+const headers = {
+  "x-hasura-admin-secret": process.env.X_HASURA_ADMIN_SECRET
+};
+
+const link = new HttpLink({
+  uri,
+  headers,
+  fetch
 });
 
-export default schema;
+export default async () => {
+  const remoteSchema = makeRemoteExecutableSchema({
+    schema: await introspectSchema(link),
+    link
+  });
+
+  const localSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+    schemaDirectives: {
+      rateLimit
+    }
+  });
+
+  const schema = mergeSchemas({
+    schemas: [remoteSchema, localSchema]
+  });
+
+  return schema;
+};
