4a3b24e800a97b54b879ec3bd5414aa2e81c72c0
Carlos Rufo <sw@carlosrj.com>
2022-09-03 04:05:07 -0500

    fix: remove remote schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,42 +2,19 @@
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
 import { rateLimit } from "../utils";
-import {
-  makeExecutableSchema,
-  makeRemoteExecutableSchema,
-  introspectSchema,
-  mergeSchemas
-} from "graphql-tools";
-import fetch from "node-fetch";
-import { HttpLink } from "apollo-link-http";
-
-const uri = process.env.X_HASURA_URL;
-const headers = {
-  "x-hasura-admin-secret": process.env.X_HASURA_ADMIN_SECRET
-};
-
-const link = new HttpLink({
-  uri,
-  headers,
-  fetch
-});
+import { makeExecutableSchema, mergeSchemas } from "graphql-tools";
 
 export default async () => {
-  const remoteSchema = makeRemoteExecutableSchema({
-    schema: await introspectSchema(link),
-    link
-  });
-
   const localSchema = makeExecutableSchema({
     typeDefs,
     resolvers,
     schemaDirectives: {
-      rateLimit
-    }
+      rateLimit,
+    },
   });
 
   const schema = mergeSchemas({
-    schemas: [remoteSchema, localSchema]
+    schemas: [localSchema],
   });
 
   return schema;
